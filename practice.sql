37d5daa9e558951ec799c33c6b07c7fdef998920
hafsatariq18 <163589125+hafsatariq18@users.noreply.github.com>
2024-07-01 22:59:31 +0530

    Added query to select patients without allergies
    Implemented SQL query to retrieve patients without recorded allergies

--- practice.sql
+++ practice.sql
@@ -2,3 +2,8 @@
 
 
 SELECT first_name,last_name,gender FROM patients WHERE gender = 'M';
+
+
+# Show first name and last name of patients who does not have allergies. (null)
+
+SELECT first_name,last_name FROM patients WHERE allergies IS NULL;
