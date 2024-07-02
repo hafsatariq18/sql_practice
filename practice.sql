a4a6e4970cf812078cdcf56ffb82033b8a2fd0a8
hafsatariq18 <163589125+hafsatariq18@users.noreply.github.com>
2024-07-02 23:26:23 +0530

    Add query to retrieve first names of patients starting with 'C'

--- practice.sql
+++ practice.sql
@@ -2,3 +2,13 @@
 
 
 SELECT first_name,last_name,gender FROM patients WHERE gender = 'M';
+
+
+# Show first name and last name of patients who does not have allergies. (null)
+
+SELECT first_name,last_name FROM patients WHERE allergies IS NULL;
+
+#Show first name of patients that start with the letter 'C'
+
+SELECT first_name FROM patients WHERE first_name LIKE "C%";
+
