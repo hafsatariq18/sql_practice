d672b05ff71b1215ed4fad3536f775178cc0906d
hafsatariq18 <163589125+hafsatariq18@users.noreply.github.com>
2024-07-03 12:21:45 +0530

    Add query to retrieve patient names within specified weight range

--- practice.sql
+++ practice.sql
@@ -11,3 +11,7 @@ SELECT first_name,last_name FROM patients WHERE allergies IS NULL;
 # Show first name of patients that start with the letter 'C'
 
 SELECT first_name FROM patients WHERE first_name LIKE "C%";
+
+#Show first name and last name of patients that weight within the range of 100 to 120 (inclusive)
+
+SELECT first_name,last_name FROM patients WHERE weight BETWEEN 100 AND 120;
