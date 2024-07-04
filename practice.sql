ecb68603d5ffac7a3a0be5676940c3241eb498c9
hafsatariq18 <163589125+hafsatariq18@users.noreply.github.com>
2024-07-04 22:09:29 +0530

    Add SQL query to concatenate first and last names into full_name

--- practice.sql
+++ practice.sql
@@ -13,3 +13,6 @@ SELECT first_name,last_name FROM patients WHERE weight BETWEEN 100 AND 120;
 
 # Update the patients table for the allergies column. If the patient's allergies is null then replace it with 'NKA'
 UPDATE patients SET allergies = 'NKA' WHERE allergies is NULL;
+
+# Show first name and last name concatinated into one column to show their full name.
+SELECT CONCAT(first_name, ' ', last_name) AS full_name FROM patients;
