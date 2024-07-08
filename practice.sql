92b511fa0ae1cc883c35b1d9a6ae0cfca27a496c
hafsatariq18 <163589125+hafsatariq18@users.noreply.github.com>
2024-07-08 22:53:58 +0530

    Add query to count total admissions for a specific patient

--- practice.sql
+++ practice.sql
@@ -36,3 +36,6 @@ SELECT COUNT(*) AS patient_id FROM admissions;
 
 #Show all the columns from admissions where the patient was admitted and discharged on the same day.
 SELECT * FROM admissions WHERE admission_date = discharge_date;
+
+#Show the patient id and the total number of admissions for patient_id 579.
+SELECT patient_id, COUNT(*) AS total_admissions FROM admissions WHERE patient_id = 579 GROUP BY patient_id;
