e28b6bd8a08e0d8c7af7b0992c83877d55cb9f20
hafsatariq18 <163589125+hafsatariq18@users.noreply.github.com>
2024-07-10 23:01:55 +0530

    Add query to retrieve unique first names from the patients table that occur only once

--- practice.sql
+++ practice.sql
@@ -51,3 +51,9 @@ SELECT first_name, last_name, allergies FROM patients where allergies IS not NUL
 
 # Show unique birth years from patients and order them by ascending.
 SELECT distinct YEAR(birth_date) AS birth_year FROM patients order by birth_date ASC;
+
+# Show unique first names from the patients table which only occurs once in the list.
+For example, if two or more people are named 'John' in the first_name column then don't include their name in the output list. 
+If only 1 person is named 'Leo' then include them in the output.
+
+SELECT distinct first_name FROM patients group by first_name having count(*) = 1;
