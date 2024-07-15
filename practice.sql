86f110203187e4975c2e9f76995006fd5d2848b7
hafsatariq18 <163589125+hafsatariq18@users.noreply.github.com>
2024-07-15 23:02:54 +0530

    Add query to find patients admitted multiple times for the same diagnosis

--- practice.sql
+++ practice.sql
@@ -86,3 +86,6 @@ ORDER BY
 allergies ASC, 
 first_name ASC, 
 last_name ASC;
+
+# Show patient_id, diagnosis from admissions. Find patients admitted multiple times for the same diagnosis.
+SELECT patient_id, diagnosis FROM admissions group by patient_id, diagnosis having count(*)>1;
