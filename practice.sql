84f271ee7fcbaa3ecface8600d8e288e410ca035
hafsatariq18 <163589125+hafsatariq18@users.noreply.github.com>
2024-07-19 23:23:22 +0530

    Add formatted patient names, ordered by first_name desc.

--- practice.sql
+++ practice.sql
@@ -105,3 +105,8 @@ GROUP BY allergies ORDER BY popularity DESC;
 # Show all patient's first_name, last_name, and birth_date who were born in the 1970s decade. Sort the list starting from the earliest birth_date.
 SELECT first_name, last_name, birth_date FROM patients where birth_date BETWEEN '1970-01-01' AND '1979-12-31' 
 order by birth_date asc
+
+# We want to display each patient's full name in a single column. Their last_name in all upper letters must appear first, then first_name in all lower case letters. Separate the last_name and first_name with a comma. Order the list by the first_name in decending order
+EX: SMITH,jane
+SELECT CONCAT(UPPER(last_name),',',LOWER(first_name))as full_name FROM patients
+order by first_name desc;
