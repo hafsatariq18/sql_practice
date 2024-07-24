bb8ee112297e4960a3251f28d6450f6384c51e5b
hafsatariq18 <163589125+hafsatariq18@users.noreply.github.com>
2024-07-24 23:14:01 +0530

    Add SQL query to retrieve patient_id, attending_doctor_id, and diagnosis for admissions matching specified criteria.

--- practice.sql
+++ practice.sql
@@ -124,3 +124,11 @@ GROUP BY day_of_month ORDER BY admission_count DESC;
 
 # Show all columns for patient_id 542's most recent admission_date.
 SELECT * FROM admissions WHERE patient_id = 542 ORDER BY admission_date DESC LIMIT 1;
+
+# Show patient_id, attending_doctor_id, and diagnosis for admissions that match one of the two criteria:
+1. patient_id is an odd number and attending_doctor_id is either 1, 5, or 19.
+2. attending_doctor_id contains a 2 and the length of patient_id is 3 characters.
+SELECT patient_id, attending_doctor_id, diagnosis 
+FROM admissions 
+WHERE (patient_id %2 !=0 AND attending_doctor_id IN (1, 5, 19))
+   OR (attending_doctor_id LIKE '%2%' AND LENGTH(patient_id) = 3);
