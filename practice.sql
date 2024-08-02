fd92a1391a8df824916e1e512357ea9e37baf6ae
hafsatariq18 <163589125+hafsatariq18@users.noreply.github.com>
2024-08-02 23:25:15 +0530

    Add query to display patients without admissions records

--- practice.sql
+++ practice.sql
@@ -193,3 +193,6 @@ SELECT
 FROM 
     patients;
 
+# Show patient_id, first_name, last_name from patients whose does not have any records in the admissions table. (Their patient_id does not exist in any admissions.patient_id rows.)
+SELECT patient_id, first_name, last_name FROM patients WHERE patient_id NOT IN (SELECT patient_id FROM admissions);
+
