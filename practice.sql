23c729e3ce5251ed1fcdef0232d52ef6010feaa5
hafsatariq18 <163589125+hafsatariq18@users.noreply.github.com>
2024-08-05 23:31:10 +0530

    Add query to display patient details and obesity status

--- practice.sql
+++ practice.sql
@@ -210,3 +210,15 @@ GROUP BY
     (weight / 10) * 10
 ORDER BY
     weight_group DESC;
+# Show patient_id, weight, height, isObese from the patients table.
+
+Display isObese as a boolean 0 or 1.
+
+Obese is defined as weight(kg)/(height(m)2) >= 30.
+
+weight is in units kg.
+
+height is in units cm.
+
+SELECT patient_id, weight, height, CASE WHEN (weight / ((height / 100.0) * (height / 100.0))) >= 30 THEN 1
+ELSE 0 END AS isObese FROM patients;
