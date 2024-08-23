c38df630287c70122f6caa8e60bb724f9855d258
hafsatariq18 <163589125+hafsatariq18@users.noreply.github.com>
2024-08-23 23:17:13 +0530

    Add query to calculate daily admissions and change from previous day

--- practice.sql
+++ practice.sql
@@ -288,3 +288,15 @@ GROUP BY
 ORDER BY
     d.doctor_id,
     year;
+
+# For each day display the total amount of admissions on that day. Display the amount changed from the previous date.
+SELECT 
+    admission_date,
+    COUNT(patient_id) AS total_admissions,
+    COUNT(patient_id) - LAG(COUNT(patient_id), 1) OVER (ORDER BY admission_date) AS change_from_previous_day
+FROM 
+    admissions
+GROUP BY 
+    admission_date
+ORDER BY 
+    admission_date;
