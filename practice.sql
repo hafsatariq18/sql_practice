95dc2529cf3f998f9678254ce991652e95267e5c
hafsatariq18 <163589125+hafsatariq18@users.noreply.github.com>
2024-08-26 10:28:54 +0530

    Add query to show customers not from specified countries

--- practice.sql
+++ practice.sql
@@ -302,3 +302,6 @@ ORDER BY
     admission_date;
 # Show the category_name and description from the categories table sorted by category_name.
 SELECT category_name, description from categories order by category_name;
+
+#Show all the contact_name, address, city of all customers which are not from 'Germany', 'Mexico', 'Spain'
+SELECT contact_name, address, city from customers where country not in ('Germany','Mexico', 'Spain');
