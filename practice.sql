0296bb74eb7a3adcca5197621077e4f10760f507
hafsatariq18 <163589125+hafsatariq18@users.noreply.github.com>
2024-08-29 23:48:29 +0530

    Add query to retrieve orders placed on a specific date

--- practice.sql
+++ practice.sql
@@ -305,3 +305,6 @@ SELECT category_name, description from categories order by category_name;
 
 #Show all the contact_name, address, city of all customers which are not from 'Germany', 'Mexico', 'Spain'
 SELECT contact_name, address, city from customers where country not in ('Germany','Mexico', 'Spain');
+
+# Show order_date, shipped_date, customer_id, Freight of all orders placed on 2018 Feb 26
+SELECT order_date, shipped_date, customer_id,freight from orders where order_date = "2018-02-26";
